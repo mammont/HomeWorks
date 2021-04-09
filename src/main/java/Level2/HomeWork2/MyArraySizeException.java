b0bd8ba560e4f8a2101610923385ac6f59420b13
mammont <mamontov_misha@mail.ru>
2021-04-09 14:52:52 +0300

    ДЗ №2 готово.

--- src/main/java/Level2/HomeWork2/MyArraySizeException.java
+++ src/main/java/Level2/HomeWork2/MyArraySizeException.java
@@ -1,7 +1,7 @@
 package Level2.HomeWork2;
 
 public class MyArraySizeException extends MyException {
-    static final int dimension = 4;
+    public static final int dimension = 4;
     MyArraySizeException(){
         super(String.format("Размерность массива должна быть %sx%s!!!", dimension, dimension));
     }
